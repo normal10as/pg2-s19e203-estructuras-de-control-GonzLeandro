e1f5fc91e243828351d5cc04dca12c0287b1c835
Leandro Joaquin Gonzalez <joayugi@gmail.com>
2019-06-12 22:15:16 -0300

    adsas

--- Estructuras_Control_Flujo/3.4.Venta/venta.vb
+++ Estructuras_Control_Flujo/3.4.Venta/venta.vb
@@ -4,18 +4,14 @@ Module venta
     Sub Main(args As String())
         Dim cantidad As Int16
         Dim precio, total As Single
-
         Console.WriteLine("Ingrese la Cantidad del producto: ")
         cantidad = Console.ReadLine()
         Console.WriteLine("Ingrese el Precio unitario del producto: ")
         precio = Console.ReadLine()
-
         total = (cantidad * precio)
         Console.WriteLine("total sin descuento: " & total)
-
         If cantidad >= 10 And cantidad <= 50 Then
-            total -= total * 0.05
-           
+            total -= total * 0.05          
         ElseIf cantidad >= 51 And cantidad <= 250 Then
             total -= total * 0.1
         ElseIf cantidad >= 251 Then
